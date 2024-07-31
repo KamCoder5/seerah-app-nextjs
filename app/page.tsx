6095fc7321fd9a1433a7d8227370710bcf569883
Tan Ahmed <tannerahmed@gmail.com>
2024-07-31 18:12:36 +0100

    fix: responsive fixes homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen flex flex-col items-center justify-center w-full">
-			<h1 className="text-5xl text-brown-500 font-bold mb-8 animate-pulse">Coming Soon</h1>
-			<p className="text-brown-500 text-lg mb-8">We are working hard to bring you something amazing. Stay tuned!</p>
+		<div className="min-h-screen flex flex-col items-center justify-center w-full p-3">
+			<h1 className="text-5xl text-brown-500 font-bold mb-8 animate-pulse text-center">Coming Soon</h1>
+			<p className="text-brown-500 text-lg mb-8 text-center">
+				We are working hard to bring you something amazing. Stay tuned!
+			</p>
 			<Link
 				href="/chapters"
 				className="bg-blue-500 p-2 text-white rounded-lg"
