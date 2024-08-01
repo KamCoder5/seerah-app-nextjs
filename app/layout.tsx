bd718de19fa46b5815cd9a22da33649e872fcd14
Tan Ahmed <tannerahmed@gmail.com>
2024-08-01 16:26:32 +0100

    chore: remove clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import { Lora } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
-
 const lora = Lora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,12 +16,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider>
-			<html lang="en">
-				<Providers>
-					<body className={cn(lora.className, "container max-w-sm m-auto")}>{children}</body>
-				</Providers>
-			</html>
-		</ClerkProvider>
+		<html lang="en">
+			<Providers>
+				<body className={cn(lora.className, "container max-w-sm m-auto")}>{children}</body>
+			</Providers>
+		</html>
 	);
 }
