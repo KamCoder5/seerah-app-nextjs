09be9daf0778facd798bbe3b02cd556794b6598f
Tan Ahmed <tannerahmed@gmail.com>
2024-08-09 17:55:41 +0100

    fix header

---
+++ app/chapters/[slug]/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+
+type LayoutProps = {
+	children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+	const pathname = usePathname();
+	const isChaptersPage = pathname === "/chapters";
+	return (
+		<>
+			<div className="container max-w-3xl m-auto">
+				{!isChaptersPage && (
+					<>
+						<header className="flex items-center text-brown-500 my-3 md:mt-12">
+							<IoIosArrowBack className="text-xl" />
+							<Link href="/chapters">Chapters</Link>
+						</header>
+						<div className="border-b border-brown-500"></div>
+					</>
+				)}
+				<main>{children}</main>
+			</div>
+		</>
+	);
+};
+
+export default Layout;
--- app/chapters/layout.tsx
+++ app/chapters/layout.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import React from "react";
-import { IoIosArrowBack } from "react-icons/io";
 
 type LayoutProps = {
 	children: React.ReactNode;
@@ -10,11 +8,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<>
 			<div className="container max-w-3xl m-auto">
-				<header className="flex items-center text-brown-500 my-3 md:mt-12">
-					<IoIosArrowBack className="text-xl" />
-					<Link href="/chapters">Chapters</Link>
-				</header>
-				<div className="border-b border-brown-500"></div>
 				<main>{children}</main>
 			</div>
 		</>
