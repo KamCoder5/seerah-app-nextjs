46df9a6c118552fbd935cf3d4709f22c13e7dc7a
KamCoder5 <kamilahmohchin@gmail.com>
2024-08-22 12:28:11 +0100

    added butto nto navbar

--- components/ui/navBar/NavBar.tsx
+++ components/ui/navBar/NavBar.tsx
@@ -3,16 +3,15 @@ import Link from "next/link";
 const NavBar = () => {
 	return (
 		<nav className="bg-white shadow-sm sticky top-0 z-50">
-			<div className="container mx-auto px-4 py-4 flex justify-between items-center">
+			<div className="container  px-4 py-4 flex justify-between items-center">
 				<div className="text-xl font-semibold">The Seerah App</div>
-
-				<div className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
-					Start learning
-					{/* <Link href="#download-section">
-						<a className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
-							Download Now
-						</a>
-					</Link> */}
+				<div className="ml-auto">
+					<Link
+						href="/chapters"
+						className="text-m font-semibold text-blue-800 hover:text-blue-900 transition duration-300 ease-in-out"
+					>
+						Start learning!
+					</Link>
 				</div>
 			</div>
 		</nav>
