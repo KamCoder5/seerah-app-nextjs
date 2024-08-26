1075c09142ea5804208ccca683f6f04b18379457
KamCoder5 <kamilahmohchin@gmail.com>
2024-08-26 12:40:09 +0100

    this will be the website we launch, just the hero as the landing page. still need to style

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import Hero from "@/components/ui/hero/Hero";
 
 export default function Home() {
 	return (
-		<>
+		<div className="overflow-hidden min-h-screen">
 			<Hero />
-			<FeaturesBlocks />
+			{/* <FeaturesBlocks />
 			<Features />
 			<BuySection />
-			<Footer />
-		</>
+			<Footer /> */}
+		</div>
 	);
 }
