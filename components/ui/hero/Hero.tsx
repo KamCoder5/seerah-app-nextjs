77755b3947c9d439f044374853f9a95751ea10fb
Tan Ahmed <tannerahmed@gmail.com>
2024-08-27 14:53:07 +0100

    fix: responsive issue + remove sticky footer

--- components/ui/hero/Hero.tsx
+++ components/ui/hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero: React.FC<Props> = (props) => {
 			<div className="container px-5 mx-auto text-center">
 				<div className="mb-6 md:mb-12 space-y-4">
 					<h4 className="text-xl font-semibold md:text-3xl">Building...</h4>
-					<h1 className="text-3xl font-bold md:text-7xl">The Seerah App</h1>
+					<h1 className="text-3xl font-bold md:text-5xl xl:text-7xl">The Seerah App</h1>
 					<p className="text-base mx-auto md:text-lg text-gray-600 md:w-2/4 xl:w-1/2 xl:text-xl">
 						Helping everyday Muslims learn and remember the life of the Prophet Muhammad ﷺ
 					</p>
@@ -22,7 +22,7 @@ const Hero: React.FC<Props> = (props) => {
 					Start reading!
 				</Link>
 
-				<div className="mx-auto w-1/2 lg:w-1/5">
+				<div className="mx-auto max-w-52 xl:max-w-72">
 					<Image
 						src="/images/template/chapter-cropped.png"
 						alt="Drone"
@@ -31,41 +31,6 @@ const Hero: React.FC<Props> = (props) => {
 						layout="responsive"
 					/>
 				</div>
-				{/* <div className="space-y-12">
-					<p className="mx-auto text-xl font-semibold text-gray-600 lg:w-3/5 sm:text-3xl">
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet obcaecati perferendis voluptas quasi
-						quidem natus error voluptatibus.
-					</p>
-					<div className="flex flex-col justify-center space-y-5 sm:flex-row sm:space-y-0 sm:space-x-10 md:space-x-20">
-						<div className="flex flex-col items-center space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4 sm:text-left">
-							<span className="flex items-center justify-center w-12 h-12 text-gray-600 bg-white rounded-full shadow">
-								<FaCamera className="w-6 h-6" />
-							</span>
-							<span className="font-medium text-gray-600">
-								Mechanical <br />
-								Shutter
-							</span>
-						</div>
-						<div className="flex flex-col items-center space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4 sm:text-left">
-							<span className="flex items-center justify-center w-12 h-12 text-gray-600 bg-white rounded-full shadow">
-								<FaMicrophoneSlash className="w-6 h-6" />
-							</span>
-							<span className="font-medium text-gray-600">
-								4dB Noise <br />
-								Reduction
-							</span>
-						</div>
-						<div className="flex flex-col items-center space-y-3 sm:flex-row sm:space-y-0 sm:space-x-4 sm:text-left">
-							<span className="flex items-center justify-center w-12 h-12 text-gray-600 bg-white rounded-full shadow">
-								<FaImage className="w-6 h-6" />
-							</span>
-							<span className="font-medium text-gray-600">
-								1-inch <br />
-								Image Sensor
-							</span>
-						</div>
-					</div>
-				</div> */}
 			</div>
 		</section>
 	);
