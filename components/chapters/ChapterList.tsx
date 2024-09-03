acfa1383e560eac674bfbbaabd41fe8887dae9ab
KamCoder5 <kamilahmohchin@gmail.com>
2024-09-03 21:27:32 +0100

    hard coded temp chapter header

--- components/chapters/ChapterList.tsx
+++ components/chapters/ChapterList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Chapter } from "@/types/chapters";
 import ChapterItem from "./ChapterItem";
+import Typography from "../ui/typography/Typography";
 
 interface ChapterListProps {
 	chapters: Chapter[];
@@ -11,6 +12,12 @@ interface ChapterListProps {
 const ChapterList: React.FC<ChapterListProps> = ({ chapters, unlockedChapters, onChapterClick }) => {
 	return (
 		<div className="my-6 w-full">
+			<Typography
+				variant="body1"
+				className="text-brown-500 mb-3 font-bold"
+			>
+				The Ancestral Legacy of Muhammad [pbuh]
+			</Typography>
 			{chapters &&
 				chapters.map((chapter, index) => (
 					<ChapterItem
