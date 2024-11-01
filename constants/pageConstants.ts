2ec07d058ed518d9f4b5688eff83be2ae8f10357
KamCoder5 <kamilahmohchin@gmail.com>
2024-11-01 20:49:37 +0000

    added feedback button

--- constants/pageConstants.ts
+++ constants/pageConstants.ts
@@ -4,6 +4,7 @@ export const HOME_CONSTANTS = {
 	PAGE_HEADING: "Chapters",
 	PAGE_DESCRIPTION: "Read each chapter, pass the quiz and progress!",
 	CHAPTER_HEADING: "Arabia Before Islam",
+	TYPEFORM_ID: "jK5VfgK6"
 };
 
 // 24 hours in milliseconds
