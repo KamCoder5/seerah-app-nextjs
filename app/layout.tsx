98e32ce319afbfd0761eaae567de1cc1239638fe
Tan Ahmed <tannerahmed@gmail.com>
2024-11-10 15:50:36 +0000

    chore: update meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,19 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 const lora = Lora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "The Seerah App",
-	description: "The Seerah App",
+	title: "The Seerah App – Learn the Life of Prophet Muhammad ﷺ",
+	description:
+		"An app that helps Muslims learn and remember the Seerah through simple, engaging chapters. Join the waitlist to stay updated and try the demo!",
+	keywords: [
+		"The Seerah App",
+		"Prophet Muhammad",
+		"Seerah",
+		"Islamic learning app",
+		"Muslim app",
+		"Seerah chapters",
+		"Prophet's life",
+		"SunnahApps",
+	],
 };
 
 export default function RootLayout({
